714ad208a2b54e26d999c442f70a029761b6176e
Michael <mike.lvaks@gmail.com>
2023-08-23 23:40:53 -0400

    Add price tracking and fix some UI
    - adds price tracking feature entry
    - fixes some UI weirdness from current price shifting graph

--- app.py
+++ app.py
@@ -16,29 +16,45 @@ def get_current_price():
     current_price = stock_info.history(period="1d")["Close"][0]
     return jsonify({"current_price": current_price})
 
+def generate_chart(symbol):
+    stock_info = yf.Ticker(symbol)
+    data = stock_info.history(period="max")
+    
+    if not data.empty:
+        data['Gap'] = data['Open'] - data['Close'].shift(1)
+        
+        chart = go.Figure(data=[go.Candlestick(x=data.index,
+                                                open=data['Open'],
+                                                high=data['High'],
+                                                low=data['Low'],
+                                                close=data['Close'])])
+        chart.add_trace(go.Scatter(x=data.index, y=data['Gap'], mode='lines', name='Gap'))
+        return chart.to_html()
+    
+    return ""
+
+def check_target_price(current_price, target_price):
+    if current_price >= target_price:
+        print(f"Target price of ${target_price} reached!\nCurrent price: ${current_price}")
+        # You could also send an email or notification here if desired
+
 @app.route('/', methods=['POST', 'GET'])
 def chart():
     symbol = ""
     chart_html = ""
-    
+
     if request.method == 'POST':
         symbol = request.form['symbol']
+        target_price = request.form.get('target_price')
 
         stock_info = yf.Ticker(symbol)
         current_price = stock_info.history(period="1d")["Close"][0]
-        historical_data = stock_info.history(period="max")
 
-        if not historical_data.empty:
-            data = historical_data
-            data['Gap'] = data['Open'] - data['Close'].shift(1)
+        if target_price and target_price != "":
+            target_price = float(target_price)
+            check_target_price(current_price, target_price)
 
-            chart = go.Figure(data=[go.Candlestick(x=data.index,
-                                                    open=data['Open'],
-                                                    high=data['High'],
-                                                    low=data['Low'],
-                                                    close=data['Close'])])
-            chart.add_trace(go.Scatter(x=data.index, y=data['Gap'], mode='lines', name='Gap'))
-            chart_html = chart.to_html()
+        chart_html = generate_chart(symbol)
 
     return render_template('index.html', symbol=symbol, chart_html=chart_html)
 
