ae9b8e00919aa529bdccc5fa346e704d3f3e1fe7
Michael <mike.lvaks@gmail.com>
2023-08-27 12:52:25 -0400

    Some clean up and heatmap

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+# app.py
+
 from flask import Flask, render_template, request, jsonify
 import yfinance as yf
+from yfinance import tickers
 import pandas as pd
 import plotly.graph_objs as go
 
@@ -80,6 +83,88 @@ def chart():
         chart_html = generate_chart(symbol)
 
     return render_template('index.html', symbol=symbol, chart_html=chart_html, current_price=current_price)
+
+@app.route('/get_heatmap_data')
+def get_heatmap_data():
+    heatmap_data = generate_heatmap_data()
+    return jsonify(heatmap_data)
+
+@app.route('/update_heatmap_data')
+def update_heatmap_data():
+    start_date = request.args.get("start_date")
+    end_date = request.args.get("end_date")
+
+    updated_heatmap_data = generate_heatmap_data(start_date, end_date)
+    return jsonify(updated_heatmap_data)
+
+def generate_heatmap_data(start_date=None, end_date=None):
+    # If start_date or end_date is not provided, default to the last 7 days
+    if not start_date:
+        start_date = (pd.Timestamp.now() - pd.DateOffset(days=7)).strftime('%Y-%m-%d')
+    if not end_date:
+        end_date = pd.Timestamp.now().strftime('%Y-%m-%d')
+
+    # Get the list of most active stocks over the last 7 days
+    most_active_stocks = get_most_active_stocks(start_date, end_date, num_stocks=200)
+    # Generate heatmap data for each sector
+    heatmap_data = []
+    for sector in set(most_active_stocks['Sector']):
+        sector_stocks = most_active_stocks[most_active_stocks['Sector'] == sector]
+        sector_heatmap = {
+            "sector": sector,
+            "stocks": []
+        }
+        for _, stock in sector_stocks.iterrows():
+            sector_heatmap["stocks"].append({
+                "symbol": stock["Symbol"],
+                "percent_change": get_percent_change(stock["Symbol"], start_date, end_date)
+            })
+        heatmap_data.append(sector_heatmap)
+    print(heatmap_data)
+    return heatmap_data
+
+# List of popular stock tickers
+popular_tickers = [
+    "AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "FB", "NVDA", "AAPL", "JPM", "JNJ",
+    "V", "MA", "UNH", "PG", "HD", "DIS", "BAC", "PYPL", "ADBE", "VZ", "CRM",
+    "TSM", "NFLX", "CMCSA", "PEP", "KO", "INTC", "CSCO", "ABBV", "XOM", "MRK",
+    "CVX", "WMT", "ABT", "NVDA", "PFE", "GOOG", "TMUS", "WFC", "BMY", "BA",
+    "ORCL", "ACN", "COST", "AVGO", "TXN", "AMGN", "NKE", "NEE", "LIN", "DHR",
+    "HON", "PM", "QCOM", "UNP", "TM", "MMM", "GE", "SBUX", "NOW", "TMO", "IBM",
+    "LOW", "C", "VZ", "INTU", "BA", "CAT", "T", "DIS", "MCD", "LMT", "BUD",
+    "JPM", "AMT", "MO", "ABT", "UPS", "USB", "AMD", "MS", "ATVI", "ADP", "DHR",
+    "TXN", "PNC", "PLD", "TGT", "BLK", "MDT", "AAPL", "MSFT", "GOOGL", "SPY"
+]
+
+def get_most_active_stocks(start_date=None, end_date=None, num_stocks=100):
+    stock_data = []
+    for ticker in popular_tickers:
+        try:
+            stock = yf.Ticker(ticker)
+            stock_info = stock.info
+            if "volume" in stock_info and stock_info["volume"] is not None:
+                stock_data.append({
+                    "Symbol": ticker,
+                    "Sector": stock_info.get("sector", "Unknown"),  # Get sector information or use "Unknown"
+                    "Volume": stock_info["volume"]
+                })
+        except Exception as e:
+            print("Error for", ticker, e)
+
+    most_active_stocks = pd.DataFrame(stock_data)
+    print(most_active_stocks)
+    return most_active_stocks
+
+
+def get_percent_change(symbol, start_date, end_date):
+    stock = yf.Ticker(symbol)
+    historical_data = stock.history(period="1d", start=start_date, end=end_date)
+    if not historical_data.empty:
+        start_price = historical_data["Close"][0]
+        end_price = historical_data["Close"][-1]
+        percent_change = ((end_price - start_price) / start_price) * 100
+        return percent_change
+    return 0.0
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
--- static/scripts.js
+++ static/scripts.js
@@ -1,33 +1,16 @@
 /* scripts.js */
-
 document.addEventListener("DOMContentLoaded", function() {
-    const chartForm = document.getElementById("chart-form");
+    // Event listener for the "Update Heatmap" button
+    document.getElementById("update-heatmap").addEventListener("click", function() {
+        const startDate = document.getElementById("start-date").value;
+        const endDate = document.getElementById("end-date").value;
 
-    chartForm.addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent default form submission
-
-        // Fetch current price and update the page
-        getCurrentPrice();
-
-        // Disable the Generate Chart button and show loading indicator
-        document.getElementById("generate_button").disabled = true;
-        document.getElementById("loading_indicator").classList.remove("hidden");
-
-        // Add animation class to other fields
-        const inputContainers = document.querySelectorAll(".input-container");
-        inputContainers.forEach(container => {
-            container.classList.add("animated");
-        });
-
-        // Remove animation class after a delay
-        setTimeout(() => {
-            inputContainers.forEach(container => {
-                container.classList.remove("animated");
-            });
-        }, 500);
+        // Update the heatmap based on the selected date range
+        updateHeatmap(startDate, endDate);
     });
 
     // Fetch current price and update slider value on page load
+    getHeatmapData();
     getCurrentPrice();
 });
 
@@ -39,12 +22,11 @@ function getCurrentPrice() {
     fetch(`/get_current_price?symbol=${symbol}`)
         .then(response => response.json())
         .then(data => {
-            document.getElementById("current_price").innerText = `Current Price: $${data.current_price}`;
             document.getElementById("target_price").max = data.current_price * 10;
             document.getElementById("target_price").value = data.current_price; // Set target price as current value
             updateSliderValue(data.current_price);
-            checkTargetPrice(data.current_price);
-            document.getElementById("current_price_container").classList.remove("hidden");
+            // checkTargetPrice(data.current_price);
+            // document.getElementById("current_price_container").classList.remove("hidden");
         })
         .catch(error => console.error('Error:', error));
 }
@@ -55,24 +37,41 @@ function updateSliderValue(value) {
     document.getElementById("slider_value").textContent = value;
 }
 
-// Event listener for the Generate Chart button
-document.getElementById("generate_button").addEventListener("click", function() {
-    getCurrentPrice();
-    // Disable the Generate Chart button and show loading indicator
-    document.getElementById("generate_button").disabled = true;
-    document.getElementById("loading_indicator").classList.remove("hidden");
+// Function for fetching heatmap data
+function getHeatmapData() {
+    // Fetch heatmap data using an API endpoint (implement in your Flask app)
+    fetch("/get_heatmap_data")
+        .then(response => response.json())
+        .then(data => {
+            // Create the heatmap using Plotly
+            const heatmap = createHeatmap(data);
 
-    // Add animation class to other fields
-    const inputContainers = document.querySelectorAll(".input-container");
-    inputContainers.forEach(container => {
-        container.classList.add("animated");
-    });
+            // Render the heatmap in the specified container
+            const heatmapContainer = document.getElementById("heatmap-div");
+            Plotly.newPlot(heatmapContainer, heatmap);
+        })
+        .catch(error => console.error("Error:", error));
+}
 
-    // Remove animation class after a delay
-    setTimeout(() => {
-        inputContainers.forEach(container => {
-            container.classList.remove("animated");
-        });
-    }, 500);
-});
+// Function for updating the heatmap based on the selected date range
+function updateHeatmap(startDate, endDate) {
+    // Fetch updated heatmap data using the selected date range
+    fetch(`/update_heatmap_data?start_date=${startDate}&end_date=${endDate}`)
+        .then(response => response.json())
+        .then(data => {
+            // Update the heatmap using Plotly
+            const heatmapContainer = document.getElementById("heatmap-div");
+            Plotly.react(heatmapContainer, createHeatmap(data));
+        })
+        .catch(error => console.error("Error:", error));
+}
 
+// Function for creating the heatmap using Plotly
+function createHeatmap(data) {
+    // Customize the heatmap creation using Plotly
+    // You'll need to structure your data appropriately for the heatmap
+    const heatmap = {
+        // ... Plotly heatmap configuration ...
+    };
+    return heatmap;
+}
